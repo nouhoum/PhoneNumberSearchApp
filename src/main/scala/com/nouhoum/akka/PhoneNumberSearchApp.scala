a0766646c859819907ac02353eee4bdc6747e718
Nouhoum TRAORE <nouhoum.traore@gmail.com>
2011-05-25 18:30:22 +0200

    Cleanup

--- src/main/scala/com/nouhoum/akka/PhoneNumberSearchApp.scala
+++ src/main/scala/com/nouhoum/akka/PhoneNumberSearchApp.scala
@@ -1,8 +1,7 @@
 package com.nouhoum.akka
 
 import akka.routing.Routing.Broadcast
-import akka.dispatch.CompletableFuture
-import akka.dispatch.Future
+import akka.dispatch.{CompletableFuture, Future}
 import scala.io.Source
 import akka.routing.{ CyclicIterator, Routing }
 import akka.actor.{ Actor, ActorRef, PoisonPill }
@@ -34,19 +33,24 @@ object PhoneNumberSearchApp extends App {
         fileContent = Source.fromFile(file).mkString
       } yield Task(fileContent)
 
-    println("Task as String ==> " + tasks.toString)
-    val master = actorOf(new Master(tasks)).start()
+    val master = actorOf(new Master(tasks.length)).start()
 
     println("Waiting for the master response....")
     val response = master !! tasks
 
-    println("Response arrived!!! = " + response + 
-        "\n====The following numbers has been found==== \n")
+    println("====The following numbers has been found====")
+    
     response match {
-    	case Some(numbers:Set[Any]) =>
-    		numbers foreach {println(_)}
+    	case Some(numbers) =>
+    	  numbers match {
+    	    case numbersAsIter:Iterable[Any] =>
+       	    println(numbersAsIter mkString("\n"))
+       	  case _ @response =>
+       	    println("Unknown response format : " + response)
+    	  }    		
     	case None => println("No phone number found")
     }
+    
     println("Total time : " + (System.currentTimeMillis - start) + " ms")
     println("=========================")
   }
@@ -55,27 +59,24 @@ object PhoneNumberSearchApp extends App {
   case class Task(fileContent: String) extends Message
   case class Result(numbers: Set[String]) extends Message
 
-  class Master(tasks: Seq[Task]) extends Actor {
+  class Master(nbOfWorkers: Int) extends Actor {
     var responseCount = 0
-    var sender: Option[CompletableFuture[Any]] = _
+    var senderFuture: Option[CompletableFuture[Any]] = _
     var phoneNumbers = Set[String]()
     //We create and start worker actors. 
     //An actor for parsing each file content
-    val taskNumber = tasks.length
-    val workers = Vector.fill(taskNumber)(actorOf[Worker].start())
+    val workers = Vector.fill(nbOfWorkers)(actorOf[Worker].start())
 
     //We create the load balancer with a round round robin strategy
     val loadBalancer = Routing.loadBalancerActor(CyclicIterator(workers))
 
     def receive = {
-      case Result(numbers) =>
-        println("Received result = " + numbers)
-        numbers foreach { println(_) }
-        phoneNumbers = phoneNumbers ++ numbers
+      case Result(intermediateNumbers) =>
+        phoneNumbers = phoneNumbers ++ intermediateNumbers
         responseCount += 1
 
-        if (responseCount == taskNumber) {
-          sender.foreach(_.completeWithResult(phoneNumbers))
+        if (responseCount == nbOfWorkers) {
+          senderFuture.foreach(_.completeWithResult(phoneNumbers))
           loadBalancer ! Broadcast(PoisonPill)
           loadBalancer ! PoisonPill
           self.stop()
@@ -83,8 +84,8 @@ object PhoneNumberSearchApp extends App {
 
       case tasks: Array[Task] =>
         println("Received task list size = " + tasks.length)
-        tasks foreach { println(_) }
-        sender = self.senderFuture
+        //tasks foreach { println(_) }
+        senderFuture = self.senderFuture
 
         for (task <- tasks) {
           loadBalancer ! task
@@ -92,8 +93,12 @@ object PhoneNumberSearchApp extends App {
         }
     }
     
+    override def preStart() = {
+      println("=> preStart() of the Master")
+    }
+    
     override def postStop() = {
-    	println("=> Master stopped!")
+    	println("=> postStop() of the Master")
     }
   }
 
@@ -110,7 +115,7 @@ object PhoneNumberSearchApp extends App {
 	
     def receive = {
       case Task(fileContent) =>
-        println("Doing hard work!!...")
+        println("Worker doing hard work!!...")
         var phoneNumbers = Set[String]()
         for(phoneNumber <- phoneRegex findAllIn fileContent) {
         	phoneNumbers += phoneNumber
@@ -119,11 +124,11 @@ object PhoneNumberSearchApp extends App {
     }
 
     override def preStart() = {
-      println("=> Worker pre start step!!")
+      println("=> preStart() of the Worker")
     }
-
+    
     override def postStop() = {
-      println("=> Worker post stop step!!")
+      println("=> postStop() of the Worker")
     }
   }
 }
